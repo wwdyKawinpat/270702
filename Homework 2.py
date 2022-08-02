5951411383ecaf4f95fb8b3ffe92578a2a32436c
wwdyKawinpat <kawinpat.kit@gmail.com>
2022-08-02 17:34:07 +0700

    Homework 4 - Advance pile stick game

--- Homework 2.py
+++ Homework 2.py
@@ -1,5 +1,5 @@
-from ast import Break
 import random
+z = random.randint(1,2)
 
 x = int(input("How many stick (N) in pile ?: "))
 while x<0:
@@ -8,47 +8,74 @@ while x<0:
         x = int(input("How many sticks (N) in the pile: "))
     elif x>0:
         x = int(input("How many sticks (N) in the pile: "))
-
+    
+        
 print("There are ",x," sticks in the pile")
-name = input("What is your name: ")
-name2 = input("what is your name: ")
-y = int(input(name+", How many sticks you will take (1 or 2): "))
-count = 0
-
-for i in range(10):
-    z = random.randint(1,2)
-
-while x>0:
-    if x==1 and y==x:
-        z==0
-        print(name+ " take ",z," sticks.")
-        print(name+" is Loser")
-        break
-    if 0<x<=3:
-        z=x-y
-        print(name2+ " take ",z," sticks.")
-        print(name2+" is Loser")
-        break
-    if y> 2:
-        print("No you cannot take more than 2 sticks!")
-        y = int(input(name+", How many sticks you will take (1 or 2): "))
-    elif y<=0:
-        print("No you cannot take more less than 1 stick!")
-        y = int(input(name+", How many sticks you will take (1 or 2): "))
-    elif y<=2:
-        for i in range(10):
-            z = random.randint(1,2)
-        x=x-y-z     
-        print(name2+ " take ",z," sticks.")
-        count += 1
-        if x>0:
-            print("There are", x," left in the pile.")
-            y = int(input(name+", How many sticks you will take (1 or 2): "))
-        elif x<0:
-            print("There are no enough sticks to take.")
-            y = int(input(name+", How many sticks you will take (1 or 2): "))
-        elif x==0:
-            print(name2+" is Loser")
+name = input("What is your name: \n")
+y = 0
+bot = 0
+i = 0
+z = 0
 
 
+if (x%3)!=1:
+    z = 2
+    print("Computer will go first\n") 
+else:
+    z = 1
+    print(name+" will go first\n") 
+while x>0:        
+    if z==1:
+     y = int(input(name+", How many sticks you will take (1 or 2): ")) 
+     if y == 1:
+      print(name+" take",y," stick.")
+     else:
+      print(name+" take",y," sticks.")
+     x -= y
+     win = "Computer is a winner."
+     z = 2 
+    elif z == 2:
+     print("There are",x,"left in pile.")
+     if (x%3) == 2:
+         bot = 1    
+     elif x == 1:
+         bot = 1         
+     elif (x%3) == 0:
+         bot = 2
+     else:
+         bot = random.randint(1,2)  
+     x -= bot
+     print("Computer take",bot,"stick.")
+     win = name+" is a winner"
+     z = 1
+            
+    if y>= 3:
+     print("No you cannot take more than 2 sticks!")
+     x+=y #reture stick that is taken to x
+     z = 1
+    elif (y<=0) and i==1:
+     print("No you cannot take more less than 1 stick!")
+     x+=y #add minus value
+     z = 1
+    elif x<0:
+     print("There are no enough sticks to take. Try again.")
+     x+=y #reture stick that is taken to x
+     z = 1
+    elif ((y or bot)==1 or (y  or bot)==2) and x ==0 :
+     print("the winner is :",win)         
+    elif ((y or bot) ==2) and x <0 :
+     print("Again pls\n")
+     if y==2 and z==2:
+      print("Again pls\n")
+      z = 1
+     elif bot==2 and z==1:
+      print("Again pls\n")
+      x+=bot
+      z = 2  
+    elif ((y or bot)==1 or (y  or bot)==2) and x >0:
+     print("There are ",x,"in the pile now\n")
+    i+=1
+
+
+            
 #print("OK. There is no stick left in the pile. You spent",count, "times.")
